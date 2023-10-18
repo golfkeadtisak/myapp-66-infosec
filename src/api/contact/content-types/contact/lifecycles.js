fcdec78058766c8fb3e661c80c59080d261412a1
= <=>
2023-10-18 11:07:08 +0700

    update decode

---
+++ src/api/contact/content-types/contact/lifecycles.js
@@ -0,0 +1,17 @@
+module.exports = {
+   // async beforeCreate(event) {
+   //     // console.log('beforeSave ', event)
+   //     event.params.data.mobile = btoa(event.params.data.mobile)
+   //     event.params.data.cardId = btoa(event.params.data.cardId)
+   //     event.params.data.line = btoa(event.params.data.line)
+   // },
+
+
+   async afterFindMany(event) {
+      await event.result.map(item => {
+            console.log('item ', item)
+            item.mobile = atob(item.mobile)
+            return item
+      })
+   },
+};
