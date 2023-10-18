ea08cae5063bd93380bcc394e6664b1f47bc44d5
= <=>
2023-10-18 11:36:54 +0700

    update decode

--- src/api/contact/content-types/contact/lifecycles.js
+++ src/api/contact/content-types/contact/lifecycles.js
@@ -1,10 +1,10 @@
 module.exports = {
-   // async beforeCreate(event) {
-   //     // console.log('beforeSave ', event)
-   //     event.params.data.mobile = btoa(event.params.data.mobile)
-   //     event.params.data.cardId = btoa(event.params.data.cardId)
-   //     event.params.data.line = btoa(event.params.data.line)
-   // },
+   async beforeCreate(event) {
+        // console.log('beforeSave ', event)
+        event.params.data.mobile = btoa(event.params.data.mobile)
+        event.params.data.cardId = btoa(event.params.data.cardId)
+        event.params.data.line = btoa(event.params.data.line)
+    },
 
 
    async afterFindMany(event) {
