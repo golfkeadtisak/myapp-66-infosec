f358f623302fc59f995c55faf24325f2fa06c918
= <=>
2023-10-18 12:06:15 +0700

    update decode

--- src/api/contact/content-types/contact/lifecycles.js
+++ src/api/contact/content-types/contact/lifecycles.js
@@ -1,26 +1,17 @@
 module.exports = {
-   async beforeCreate(event) {
-        // console.log('beforeSave ', event)
-        event.params.data.mobile = btoa(event.params.data.mobile)
-        event.params.data.cardId = btoa(event.params.data.cardId)
-        event.params.data.line = btoa(event.params.data.line)
-    },
+   // async beforeCreate(event) {
+   //     // console.log('beforeSave ', event)
+   //     event.params.data.mobile = btoa(event.params.data.mobile)
+   //     event.params.data.cardId = btoa(event.params.data.cardId)
+   //     event.params.data.line = btoa(event.params.data.line)
+   // },
 
-    async afterFindOne(event){
-      //console.log(event)
-      if(!event.result) return
-      event.params.data.mobile = atob(event.result.data.mobile)
-      event.params.data.cardId = atob(event.result.data.cardId)
-      event.params.data.line = atob(event.result.data.line)
- },
 
- async afterFindMany(event){
-   console.log(event)
-   if(!event.result) return
-   await event.result.forEach(item => {
-      item.mobile = atob(item.mobile)
-      item.cardId = atob(item.cardId)
-      item.line =   atob(item.line)
+   async afterFindMany(event) {
+      await event.result.map(item => {
+            console.log('item ', item)
+            item.mobile = atob(item.mobile)
+            return item
       })
    },
 };
